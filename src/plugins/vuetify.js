0252efffcfc82636df36525cda6a0ee13c295a27
Ravishan Balasooriya <ravimuay@gmail.com>
2022-02-25 12:49:14 +0530

    Added Navbar component

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,13 @@
+//import '@fontawesome/fontawesome-free/css/all.css';
+import 'material-design-icons-iconfont';
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib/framework';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+
+    icons:{
+        iconfont:'md' || 'fa' || 'md'
+    }
+});
